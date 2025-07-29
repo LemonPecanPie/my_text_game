36dfe81a46525436d2aeba3b4d9c1e172599e0d8
Aidan Paul <aidanhpaul@gmail.com>
2025-07-29 16:27:04 -0400

    Implement hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use cursive::views::{Dialog, TextView};
-
 fn main() {
     // Creates the cursive root - required for every application.
     let mut siv = cursive::default();
 
-    // Creates a dialog with a single "Quit" button
-    siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
-                         .title("Cursive")
-                         .button("Quit", |s| s.quit()));
+    siv.add_global_callback('q', cursive::Cursive::quit);
 
-    // Starts the event loop.
+    siv.add_layer(cursive::views::TextView::new(
+        "Hello, world!\n\
+         Press 'q' to quit the application.",
+    ));
+
     siv.run();
 }
